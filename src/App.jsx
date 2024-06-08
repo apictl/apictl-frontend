b2089840835f0f8507e48916e8bb24eb4daeab28
Manan Gandhi <ardumanan@gmail.com>
2024-06-08 22:33:41 +0530

    Feat: Refined coming soon page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="window">
-      <h2 className="header-text">API Gateway</h2>
+      <div className="logo" />
+      <h2 className="header-text">Aegis Gateway</h2>
       <h2 className="header-subtext">Coming soon</h2>
     </div>
   );
